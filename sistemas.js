1d6bcd638548fe6d355162dbc72e7c6c937a47eb
JuanHedz <72885736+JuanHedz@users.noreply.github.com>
2020-10-14 13:19:06 -0500

    Add files via upload

---
+++ sistemas.js
@@ -0,0 +1,9 @@
+var texto= document.getElementById("resumen");
+var boton= document.getElementById("botoncito");
+boton.addEventListener("click", dibujoporClick);
+
+
+function dibujoporClick()
+{
+alert("hola");
+}
